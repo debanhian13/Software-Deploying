6cb5e01f0fa85ffa809fc0bd6dea7d046281bab4
abrahamjso <abrahamjso@gmail.com>
2017-11-04 11:40:55 -0600

    seguimiento del scrapper

--- scraper.js
+++ scraper.js
@@ -1,32 +1,45 @@
 let axios = require('axios');
 let cheerio = require('cheerio');
 let fs = require('fs');
-var ABC = [];
+const ABC = [];
+const BASE_URL = 'https://www.lyrics.com/';
 
 for (var index = 65; index <= 90; index++) {
   ABC.push(String.fromCharCode(index));
 }
 
-
-
-for (var i = 0; i < 26; i++) {
-  var name;
-  var json = { artists: { name: "" } };
-  var base_url = 'https://www.lyrics.com/artists/' + ABC[i] + '/99999';
-  axios.get(base_url).then((response) => {
-    var $ = cheerio.load(response.data);
-
-    $('.tdata tbody tr td strong').each((i, elm) => {
-      name = $(elm).children().eq(0).text();
-      json.name = name;
-      console.log(json);
-    });
-  })
-  fs.writeFile(ABC[i] + '.json', JSON.stringify(json, null, 4), function (err) {
-    console.log('Archivo creado');
-  })
+function getArtistas (ABC) {
+  for (var i = 0; i < ABC.length; i++) {
+    const alphabet = ABC[i]
+    var base_url = 'https://www.lyrics.com/artists/' + ABC[i] + '/99999';
+    const alpha = axios.get(base_url).then((response) => {
+      const objPorLetra = {
+        letra: alphabet,
+        artistas: []
+      };
+  
+      var $ = cheerio.load(response.data);
+      $('.tdata tbody tr td strong').each((i, elm) => {
+        const name = $(elm).children().eq(0).text();
+        const url = $(elm).find('a').attr('href');
+        const objArtista = {
+          name: name,
+          urlToExtractAlbum: BASE_URL + url
+        }
+        objPorLetra.artistas.push(objArtista)
+      });
+      fs.writeFile('statics/' + alphabet + '.json', JSON.stringify(objPorLetra, null, 4), function (err) {
+        console.log('Archivo: ' + alphabet + '.json creado, contiene: ' + objPorLetra.artistas.length);
+      })
+    })
+  }
 }
 
+function getAlbumsByArtist(objArtista) {
+
+}
+
+getArtistas(ABC)
 
 
 
