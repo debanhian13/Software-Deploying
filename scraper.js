08d338949791813fcaa431fe785b3b5eb38c19e8
NikkaDz <btc7334@hotmail.com>
2017-11-10 16:06:20 -0600

    Adding functions

--- scraper.js
+++ scraper.js
@@ -1,3 +1,4 @@
+
 let axios = require('axios');
 let cheerio = require('cheerio');
 let fs = require('fs');
@@ -26,20 +27,66 @@ function getArtistas (ABC) {
           name: name,
           urlToExtractAlbum: BASE_URL + url
         }
-        objPorLetra.artistas.push(objArtista)
-      });
+        objPorLetra.artistas.push(objArtista);
+        });
       fs.writeFile('statics/' + alphabet + '.json', JSON.stringify(objPorLetra, null, 4), function (err) {
-        console.log('Archivo: ' + alphabet + '.json creado, contiene: ' + objPorLetra.artistas.length);
+       console.log('Archivo: ' + alphabet + '.json creado, contiene: ' + objPorLetra.artistas.length);
       })
+
     })
   }
 }
 
 function getAlbumsByArtist(objArtista) {
+    for(var i=0; i< objArtista.urlToExtractAlbum.length; i++){
+      const alpha = axios.get(objArtista.urlToExtractAlbum).then((response) => {
+      const objPorArtista = {
+        artista: objArtista.name,
+        albums: []
+      };
 
+      var $ = cheerio.load(response.data);
+      $('.tdata-ext .clearfix .artist-album-label').each((i, elm) => {
+        const name = $(elm).children().eq(0).text();
+        const url = $(elm).find('a').attr('href');
+        const objAlbum = {
+          name: name,
+          urlToExtractSongs: BASE_URL + url
+        }
+        objPorArtista.albums.push(objAlbum)
+      });
+
+      console.log(objPorArtista);
+
+    })
+  }
 }
 
-getArtistas(ABC)
+function getSongsByAlbums(objAlbum) {
+    for(var i=0; i< objAlbum.urlToExtractAlbum.length; i++){
+      const alpha = axios.get(objAlbum.urlToExtractSongs).then((response) => {
+      const objPorAlbum = {
+        album: objAlbum.name,
+        songs: []
+      };
 
+      var $ = cheerio.load(response.data);
+      $('.tdata tbody tr td strong').each((i, elm) => {
+        const name = $(elm).children().eq(0).text();
+        const url = $(elm).find('a').attr('href');
+        const objSong = {
+          name: name,
+          urlOfLyric: BASE_URL + url
+        }
+        objPorAlbum.songs.push(objSong)
+      });
 
+      console.log(objPorAlbum);
 
+    })
+  }
+}
+
+getArtistas(ABC);
+getAlbumsByArtist(objArtista);
+getSongsByAlbums(objAlbum);
